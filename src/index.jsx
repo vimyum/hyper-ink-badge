3ca2f82909cefc48b4076e68ba93dc6f6df6027b
Takahiro <takahiro@TakahironoMacBook-Pro.local>
2018-06-17 00:03:24 +0900

    add formatter

--- src/index.jsx
+++ src/index.jsx
@@ -2,35 +2,35 @@ import InkSvg from 'react-svg-loader!./ink_s.svg';
 
 const baseColor = ['#010101', '#a0a0a0'];
 const colorTmpl = [
-	['#F0297E'/* red */, '#12D812' /* green */],
-	['#F6CB45'/* yellow */, '#9734D9' /* purple */],
-	['#69D2E7'/* cyan */, '#DD417D' /* pink */],
-	['#9734D9'/* purple */, '#65D344' /* green */],
-	['#BFF1E5'/* white */, '#E59D0D' /* orange */],
+  ['#F0297E' /* red */, '#12D812' /* green */],
+  ['#F6CB45' /* yellow */, '#9734D9' /* purple */],
+  ['#69D2E7' /* cyan */, '#DD417D' /* pink */],
+  ['#9734D9' /* purple */, '#65D344' /* green */],
+  ['#BFF1E5' /* white */, '#E59D0D' /* orange */],
 ];
 
 const styles = {
-    img : {
-        zIndex: 20,
-        position: "fixed",
-        bottom: "25px",
-        right: "0px",
-        cursor: "pointer",
-    },
-}
+  img: {
+    zIndex: 20,
+    position: 'fixed',
+    bottom: '25px',
+    right: '0px',
+    cursor: 'pointer',
+  },
+};
 
-exports.decorateConfig = (config) => {
+exports.decorateConfig = config => {
   return Object.assign({}, config, {
     css: `
       ${config.css || ''}
-	    `
+	    `,
   });
-}
+};
 
-exports.middleware = store => next => (action) => {
-    // TBD
-    next(action);
-}
+exports.middleware = store => next => action => {
+  // TBD
+  next(action);
+};
 
 exports.decorateTerm = (Term, { React, notify }) => {
   return class extends React.Component {
@@ -45,10 +45,10 @@ exports.decorateTerm = (Term, { React, notify }) => {
         stickToTitile: false,
         colorsOfTitle: {},
         tmplIdx: 1,
-      }
+      };
       props.onActive = () => {
         console.log('onActive is Called!!');
-      }
+      };
     }
 
     onChangeColor() {
@@ -68,12 +68,12 @@ exports.decorateTerm = (Term, { React, notify }) => {
       if (this.clickCount < 2) {
         console.log('single click');
         this.prevColors = [...this.state.colors];
-        const e = document.querySelectorAll("use[fill]");
+        const e = document.querySelectorAll('use[fill]');
         if (e) {
           e.forEach(el => {
             console.log('initialize..');
             el.removeAttribute('data-filled');
-          })
+          });
         }
 
         const idx = this.state.tmplIdx;
@@ -96,26 +96,29 @@ exports.decorateTerm = (Term, { React, notify }) => {
     }
 
     setColorByTerm(uid) {
-      console.log('setColorByTerm is called.')
-      const e = document.querySelectorAll("use[fill]");
+      console.log('setColorByTerm is called.');
+      const e = document.querySelectorAll('use[fill]');
       if (!e) {
         console.info('No use[fill].');
         return;
       }
       e.forEach(el => {
-      	if (el.getAttribute('data-filled') == 'true') {
+        if (el.getAttribute('data-filled') == 'true') {
           console.log('data-filled is true, and retrun');
           return;
         }
         const color = el.getAttribute('fill');
-      	if ( color == this.prevColors[0] || color == baseColor[0]) {
+        if (color == this.prevColors[0] || color == baseColor[0]) {
           el.setAttribute('fill', this.state.colors[0]);
           console.log('set to color-0');
-        } else if ( color == this.prevColors[1] || color == baseColor[1]) {
+        } else if (color == this.prevColors[1] || color == baseColor[1]) {
           el.setAttribute('fill', this.state.colors[1]);
-      	  console.log('set to color-1');
+          console.log('set to color-1');
         } else {
-          console.log('NOT match to previous color: %o', el.getAttribute('fill'));
+          console.log(
+            'NOT match to previous color: %o',
+            el.getAttribute('fill')
+          );
         }
         el.setAttribute('data-filled', 'true');
       });
@@ -137,20 +140,24 @@ exports.decorateTerm = (Term, { React, notify }) => {
       const { uid, isTermActive, term } = this.props;
       if (isTermActive) {
         console.log('rendered: %o', this.props.uid);
-        console.log('state: %o', this.state);
       }
 
       const children = [
-        React.createElement(Term, Object.assign({}, this.props, { style: { backgroundColor: 'rgba(25,64,25,0.5)' } }))
+        React.createElement(
+          Term,
+          Object.assign({}, this.props, {
+            style: { backgroundColor: 'rgba(25,64,25,0.5)' },
+          })
+        ),
       ];
       if (isTermActive) {
-        children.unshift(<div onClick={this.onChangeColor.bind(this)}
-          style={styles.img}>
-          <InkSvg />
-        </div>);
+        children.unshift(
+          <div onClick={this.onChangeColor.bind(this)} style={styles.img}>
+            <InkSvg />
+          </div>
+        );
       }
-
-      return <div style={{ width: '100%', height: '100%' }}>{children}</div>
+      return <div style={{ width: '100%', height: '100%' }}>{children}</div>;
     }
   };
-}
+};
